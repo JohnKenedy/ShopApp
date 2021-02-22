965daae9dd88638056933b99486fd9ffe0909fa1
John Kenedy <john.kenedy0410@gmail.com>
2021-02-22 15:41:34 -0300

    LoginActivity.kt, RegisterActivity.kt

--- app/src/main/java/com/canytech/shopapp/activities/LoginActivity.kt
+++ app/src/main/java/com/canytech/shopapp/activities/LoginActivity.kt
@@ -2,17 +2,17 @@ package com.canytech.shopapp.activities
 
 import android.content.Intent
 import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowInsets
 import android.view.WindowManager
-import androidx.appcompat.app.AppCompatActivity
 import com.canytech.shopapp.R
 
-class SplashActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_login)
 
         @Suppress("DEPRECATION")
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
@@ -23,12 +23,5 @@ class SplashActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         }
-
-        Handler().postDelayed(
-            {
-                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
-                finish()
-            },2000
-        )
     }
 }
